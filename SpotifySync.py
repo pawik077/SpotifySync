dfad0144693ab032a92ef6bbd9bbdfe1f4284a3d
pawik077 <pawik.r.s.077@gmail.com>
2021-02-13 22:13:11 +0100

    Added authorize() function

--- SpotifySync.py
+++ SpotifySync.py
@@ -1,3 +1,6 @@
+import json
+import requests
+
 class Track:
   def __init__(self, title: str = '', artist: str = '', uri: str = '') -> None:
     self.artist = artist
@@ -13,3 +16,16 @@ class Track:
         return True
       else:
         return False
+
+def authorize() -> str:
+  url = "https://accounts.spotify.com/api/token"
+  payload = 'grant_type=refresh_token&refresh_token=' + settings['refresh_token']
+  headers = {
+    'Authorization': 'Basic ' + settings['authorization_token'],
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  response = requests.request('POST', url, headers=headers, data=payload)
+  return json.loads(response.text)['access_token']
+
+settings = json.load(open('settings.json', 'r'))
+authKey = 'Bearer ' + authorize()
